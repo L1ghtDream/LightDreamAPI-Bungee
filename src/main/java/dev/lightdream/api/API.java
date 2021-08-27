e8fdecae88ae3afb50f5d1e775caf5a75127132f
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-08-27 10:43:39 +0200

    1.27

--- src/main/java/dev/lightdream/api/API.java
+++ src/main/java/dev/lightdream/api/API.java
@@ -17,7 +17,7 @@ public final class API extends LightDreamPlugin {
     //Settings
     public final static String PROJECT_NAME = "LightDreamAPI";
     public final static String PROJECT_ID = "ld-api";
-    public final static String VERSION = "1.26";
+    public final static String VERSION = "1.27";
     public static API instance;
     public Config config;
 
