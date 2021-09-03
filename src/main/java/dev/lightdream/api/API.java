c3086a903a143ddff21a6104cb7746e905e6d66f
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-09-03 09:32:26 +0200

    1.79

--- src/main/java/dev/lightdream/api/API.java
+++ src/main/java/dev/lightdream/api/API.java
@@ -37,7 +37,7 @@ public final class API extends LightDreamPlugin {
         permission = setupPermissions();
 
         //Register
-        init("LightDreamAPI", "ld-api", "1.78");
+        init("LightDreamAPI", "ld-api", "1.79");
     }
 
     @Override
--- src/main/java/dev/lightdream/api/files/dto/PluginLocation.java
+++ src/main/java/dev/lightdream/api/files/dto/PluginLocation.java
@@ -8,6 +8,8 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 
+import java.util.Objects;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -76,5 +78,18 @@ public class PluginLocation {
                 ", y=" + y +
                 ", z=" + z +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PluginLocation that = (PluginLocation) o;
+        return Double.compare(that.x, x) == 0 && Double.compare(that.y, y) == 0 && Double.compare(that.z, z) == 0 && Objects.equals(world, that.world);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(world, x, y, z);
     }
 }
