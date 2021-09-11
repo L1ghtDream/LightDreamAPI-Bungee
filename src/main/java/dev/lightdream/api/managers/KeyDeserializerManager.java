de80cccae3cb46c852a1156fb27017d04f1bbece
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-09-11 09:23:12 +0200

    2.45

--- src/main/java/dev/lightdream/api/managers/KeyDeserializerManager.java
+++ src/main/java/dev/lightdream/api/managers/KeyDeserializerManager.java
@@ -7,7 +7,7 @@ public class KeyDeserializerManager extends KeyDeserializer {
     @Override
     public Object deserializeKey(String key, DeserializationContext ctxt) {
         System.out.println(key);
-        return null;
+        return key;
     }
 }
 
