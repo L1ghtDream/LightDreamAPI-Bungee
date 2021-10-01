c99a5bc641bf49cff8937d0eb38d5424bb531f11
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-10-01 22:09:34 +0200

    2.177

--- src/main/java/dev/lightdream/api/API.java
+++ src/main/java/dev/lightdream/api/API.java
@@ -228,7 +228,7 @@ public final class API implements IAPI {
 
     @Override
     public String getProjectVersion() {
-        return "2.176";
+        return "2.177";
     }
 
     @Override
--- src/main/java/dev/lightdream/api/gui/GUI.java
+++ src/main/java/dev/lightdream/api/gui/GUI.java
@@ -79,12 +79,8 @@ public abstract class GUI implements InventoryProvider {
                 item.item.lore = parse(item.item.lore, player);
                 item.item.headOwner = parse(item.item.displayName, player);
 
-                System.out.println(item);
-
                 item.args = parse(item.args, player);
-
-                System.out.println(item);
-
+                
                 contents.set(Utils.getSlotPosition(item.item.slot), ClickableItem.of(ItemBuilder.makeItem(item.item), e -> {
                     List<String> functions = item.getFunctions();
                     functions.forEach(function -> functionCall(player, function, item.getFunctionArgs(function)));
