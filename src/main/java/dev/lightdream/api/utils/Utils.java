86c0d231020567c3d4e10ea6c29b909813d5e937
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-11-03 20:10:55 +0100

    3.82

--- src/main/java/dev/lightdream/api/utils/Utils.java
+++ src/main/java/dev/lightdream/api/utils/Utils.java
@@ -16,6 +16,11 @@ import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
+import javax.management.Attribute;
+import javax.management.AttributeList;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -225,6 +230,34 @@ public class Utils {
         }
 
         return objects.get(index);
+    }
+
+    public static double getRam(){
+        return ((double)(Runtime.getRuntime().totalMemory()/1024)/1024) - ((double)(Runtime.getRuntime().freeMemory()/1024)/1024);
+    }
+
+    public static double getCpuLoad() {
+        try {
+            MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+            ObjectName name = ObjectName.getInstance("java.lang:type=OperatingSystem");
+            AttributeList list = mbs.getAttributes(name, new String[]{"ProcessCpuLoad"});
+
+            if (list.isEmpty())
+                return Double.NaN;
+
+            Attribute att = (Attribute) list.get(0);
+            Double value = (Double) att.getValue();
+
+            if (value == -1.0)
+                return Double.NaN;
+            return ((int) (value * 1000) / 10.0);
+        } catch (Exception e) {
+            return Double.NaN;
+        }
+    }
+
+    public static String getJava(){
+        return String.format("Java Version: %s %s. \n", System.getProperty("java.vendor"), System.getProperty("java.version"));
     }
 
 
